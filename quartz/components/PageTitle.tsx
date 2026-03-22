d94794d1bb982f6504cf71b58ad2d763e5e40e40
Aaron Gillespie <aarongilly@gmail.com>
2026-03-22 11:26:14 -0500

    Fixed margin too

--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -9,7 +9,7 @@ const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzCompo
   const imgSrc = joinSegments(baseDir, "static/datajournal.svg")
   return (
     <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir} style="display:flex; align-items: center; margin: 0px"><img src={imgSrc} alt="Logo" style="height: 1em; padding-right: 0.25em"></img>{title}</a>
+      <a href={baseDir} style="display:flex; align-items: center;"><img src={imgSrc} alt="Logo" style="height: 1em; padding-right: 0.25em;  margin: 0px"></img>{title}</a>
     </h2>
   )
 }
